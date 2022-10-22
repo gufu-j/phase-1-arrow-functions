7cba84ec5e04c4f9d0a2527c35be2668fd18d30e
diego <guatemaladiegojose@gmail.com>
2022-10-21 21:17:41 -0400

    commit tree 5895bfbd90aa1135259173d66112d703a7044375

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000, 100);
+const square = (parameter3) => parameter3*parameter3;
+const add = (parameter4, parameter5)=> parameter4+parameter5;
+add(3,4);
